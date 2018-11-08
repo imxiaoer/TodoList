69320b010c4dc24ac5f2b9479106d059a959279b
yunfeng <281542025@qq.com>
2018-11-08 13:55:49 +0800

    todolist

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="todo" @keyup.enter="addItem" class="txt" placeholder="请输入待办事项">
+    <input type="button" value="添 加" @click="addItem" class="btn">
+    <ul class="list">
+      <li
+      is="todo-item"
+      v-for="(item, index) in items"
+      :key="index"
+      :content="item"
+      @remove="removeItem(index)"></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      todo: '',
+      items: []
+    }
+  },
+  components: {
+    'todo-item': TodoList
+  },
+  methods: {
+    addItem () {
+      this.todo && this.items.push(this.todo)
+      this.todo = ''
+    },
+    removeItem (index) {
+      this.items.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #app {
+    width: 1000px;
+    margin: 200px auto;
+    text-align: center;
+  }
+  .txt {
+    width: 240px;
+    height: 40px;
+    color: #666;
+    padding: 0 10px;
+    border: 1px solid #ddd;
+    vertical-align: top;
+    outline: none;
+  }
+  .btn {
+    width: 100px;
+    height: 42px;
+    color: #fff;
+    background-color: orange;
+    border: 1px solid orange;
+    outline: none;
+    cursor: pointer;
+  }
+  .list {
+    margin-top: 20px;
+  }
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,34 @@
+<template>
+  <li class="todo-item">
+    {{content}}
+    <input type="button" class="btn-del" value="删除" @click="$emit('remove')">
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['content']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .todo-item {
+    list-style: none;
+    padding-left: 10px;
+    margin-top: 10px;
+    line-height: 40px;
+    color: #333;
+    background-color: #f5f5f5;
+    text-align: left;
+  }
+  .btn-del{
+    float: right;
+    margin: 5px 10px 0 0;
+    padding: 5px 10px;
+    background-color: #fff;
+    border: 1px solid red;
+    color: red;
+    cursor: pointer;
+  }
+</style>
